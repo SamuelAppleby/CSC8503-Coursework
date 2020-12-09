398c55322bdc273fd6c1541f3ea2f36a3f34cc5d
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2020-12-09 17:02:33 +0000

    Add project files.

---
+++ CSC8503/CSC8503Common/GameWorld.cpp
@@ -0,0 +1,134 @@
+#include "GameWorld.h"
+#include "GameObject.h"
+#include "Constraint.h"
+#include "CollisionDetection.h"
+#include "../../Common/Camera.h"
+#include <algorithm>
+
+using namespace NCL;
+using namespace NCL::CSC8503;
+
+GameWorld::GameWorld()	{
+	mainCamera = new Camera();
+
+	shuffleConstraints	= false;
+	shuffleObjects		= false;
+	worldIDCounter		= 0;
+}
+
+GameWorld::~GameWorld()	{
+}
+
+void GameWorld::Clear() {
+	gameObjects.clear();
+	constraints.clear();
+}
+
+void GameWorld::ClearAndErase() {
+	for (auto& i : gameObjects) {
+		delete i;
+	}
+	for (auto& i : constraints) {
+		delete i;
+	}
+	Clear();
+}
+
+void GameWorld::AddGameObject(GameObject* o) {
+	gameObjects.emplace_back(o);
+	o->SetWorldID(worldIDCounter++);
+}
+
+void GameWorld::RemoveGameObject(GameObject* o, bool andDelete) {
+	gameObjects.erase(std::remove(gameObjects.begin(), gameObjects.end(), o), gameObjects.end());
+	if (andDelete) {
+		delete o;
+	}
+}
+
+void GameWorld::GetObjectIterators(
+	GameObjectIterator& first,
+	GameObjectIterator& last) const {
+
+	first	= gameObjects.begin();
+	last	= gameObjects.end();
+}
+
+void GameWorld::OperateOnContents(GameObjectFunc f) {
+	for (GameObject* g : gameObjects) {
+		f(g);
+	}
+}
+
+void GameWorld::UpdateWorld(float dt) {
+	if (shuffleObjects) {
+		std::random_shuffle(gameObjects.begin(), gameObjects.end());
+	}
+
+	if (shuffleConstraints) {
+		std::random_shuffle(constraints.begin(), constraints.end());
+	}
+	for (auto& i : gameObjects) {
+		Matrix4 local = i->GetTransform().GetMatrix();
+		local.SetPositionVector({ 0, 0, 0 });
+		Vector3 fwd = local * Vector4(0, 0, -1, 1.0f);
+		Vector3 worldPos = i->GetTransform().GetPosition();
+		Debug::DrawLine(worldPos, worldPos + fwd * 2, Debug::BLUE);
+	}
+}
+
+bool GameWorld::Raycast(Ray& r, RayCollision& closestCollision, bool closestObject) const {
+	//The simplest raycast just goes through each object and sees if there's a collision
+	RayCollision collision;
+	for (auto& i : gameObjects) {
+		if (!i->GetBoundingVolume()) { //objects might not be collideable etc...
+			continue;
+		}
+		RayCollision thisCollision;
+		if (CollisionDetection::RayIntersection(r, *i, thisCollision)) {
+			if (!closestObject) {	
+				closestCollision		= collision;
+				closestCollision.node = i;
+				Debug::DrawLine(r.GetPosition(), closestCollision.collidedAt, Debug::RED, 10.0f);
+				return true;
+			}
+			else {
+				if (thisCollision.rayDistance < collision.rayDistance) {
+					thisCollision.node = i;
+					collision = thisCollision;
+				}
+			}
+		}
+	}
+	if (collision.node) {
+		closestCollision		= collision;
+		closestCollision.node	= collision.node;
+		Debug::DrawLine(r.GetPosition(), closestCollision.collidedAt, Debug::RED, 10.0f);
+		return true;
+	}
+	Debug::DrawLine(r.GetPosition(), r.GetPosition() + (r.GetDirection() * 500), Debug::RED, 10.0f);
+	return false;
+}
+
+
+/*
+Constraint Tutorial Stuff
+*/
+
+void GameWorld::AddConstraint(Constraint* c) {
+	constraints.emplace_back(c);
+}
+
+void GameWorld::RemoveConstraint(Constraint* c, bool andDelete) {
+	constraints.erase(std::remove(constraints.begin(), constraints.end(), c), constraints.end());
+	if (andDelete) {
+		delete c;
+	}
+}
+
+void GameWorld::GetConstraintIterators(
+	std::vector<Constraint*>::const_iterator& first,
+	std::vector<Constraint*>::const_iterator& last) const {
+	first	= constraints.begin();
+	last	= constraints.end();
+}
---
+++ CSC8503/CSC8503Common/Ray.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "../../Common/Vector3.h"
+#include "../../Common/Plane.h"
+
+namespace NCL {
+	namespace Maths {
+		struct RayCollision {
+			void*		node;			//Node that was hit
+			Vector3		collidedAt;		//WORLD SPACE position of the collision!
+			float		rayDistance;
+
+			RayCollision() {
+				node			= nullptr;
+				rayDistance		= FLT_MAX;
+			}
+		};
+
+		class Ray {
+		public:
+			Ray(Vector3 position, Vector3 direction) {
+				this->position  = position;
+				this->direction = direction;
+			}
+			~Ray(void) {}
+
+			Vector3 GetPosition() const {return position;	}
+			void SetPosition(Vector3 val) { position = val; }
+			Vector3 GetDirection() const {return direction;	}
+			void SetDirection(Vector3 val) { direction = val; }
+
+		protected:
+			Vector3 position;	//World space position
+			Vector3 direction;	//Normalised world space direction
+		};
+	}
+}
